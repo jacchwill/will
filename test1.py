0fb6664af8972a14e47faffcce3d2390c0eaea85
jacch.will <jacch.will@gmail.com>
2020-07-17 22:18:10 +0800

    a

--- test1.py
+++ test1.py
@@ -20,6 +20,12 @@ print (sheet["A1"].value)
 print (sheet["A2"].value)
 print (sheet["A3"].value)
 
+xls=readxls("db.xlsx")
+sheet=xls.read("咚咚鼠")  
+print (sheet["A1"].value)
+print (sheet["A2"].value)
+print (sheet["A3"].value)
+
 #工作表2
 #xls2=readxls("db.xlsx")
 #sheet=xls.read("工作表2")
